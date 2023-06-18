59253d8fad488c32b15f3d33995a1de04058e24d
shulem1997 <shalomadoram@gmail.com>
2023-06-18 17:11:03 +0300

    room

--- app/src/main/java/com/example/chat/ChatActivity.java
+++ app/src/main/java/com/example/chat/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.example.chat;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,15 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        loadMessages();
+        EditText msgContent = findViewById(R.id.etInput);
+        Button send = findViewById(R.id.btnSend);
+        send.setOnClickListener(view-> {
+
+        });
+    }
+
+    private void loadMessages() {
     }
 }
--- app/src/main/java/com/example/chat/ContactsActivity.java
+++ app/src/main/java/com/example/chat/ContactsActivity.java
@@ -11,12 +11,6 @@ import androidx.room.Room;
 import com.example.chat.databinding.ActivityContactsBinding;
 
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,8 +24,6 @@ public class ContactsActivity extends AppCompatActivity {
     private ArrayAdapter<String> adapter;
     private UserDao userDao;
     private User logged;
-    private  String token;
-    private  String chats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +34,7 @@ public class ContactsActivity extends AppCompatActivity {
 
         db = Room.databaseBuilder(getApplicationContext(), AppDB.class, "users")
                 .allowMainThreadQueries().build();
-        getChatsFormServer(getIntent().getExtras().getString("username"),getIntent().getExtras().getString("password"));
+
         userDao = db.UserDao();
         logged = userDao.get(getIntent().getExtras().getString("username"));
         handlePosts();
@@ -85,121 +77,4 @@ public class ContactsActivity extends AppCompatActivity {
 
         adapter.notifyDataSetChanged();
     }
-    private void getChatsFormServer(String username, String password){
-        getToken(username, password);
-        Thread thread = new Thread(new Runnable() {
-            private StringBuilder responseBody; // Variable to hold the response body
-
-            public StringBuilder getResponseBody() {
-                return responseBody;
-            }
-
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("http://10.0.2.2:5000/api/Chats/");
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setRequestProperty("Content-Type", "application/json");
-                    connection.setRequestProperty("Authorization", "Bearer " + token);
-
-                    int responseCode = connection.getResponseCode();
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        StringBuilder responseBody;
-                        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                            String line;
-                            StringBuilder response = new StringBuilder();
-                            while ((line = reader.readLine()) != null) {
-                                response.append(line);
-                            }
-                            chats = response.toString(); // Assign the response to the variable
-                        }
-                    } else {
-                        // Handle the error case
-                        System.out.println("HTTP GET request failed with response code: " + responseCode);
-                    }
-
-                    connection.disconnect();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println(e.toString());
-                }
-            }
-        });
-
-// Start the thread
-        thread.start();
-
-        try {
-            // Wait for the thread to finish
-            thread.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void getToken(String username, String password ) {
-        Thread thread = new Thread(new Runnable() {
-            private StringBuilder responseBody; // Variable to hold the response body
-
-            public StringBuilder getResponseBody() {
-                return responseBody;
-            }
-
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("http://10.0.2.2:5000/api/Tokens/"); // Replace with your API endpoint
-
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("POST");
-                    connection.setRequestProperty("Content-Type", "application/json");
-                    connection.setDoOutput(true);
-
-                    String requestBody = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
-
-                    try {
-                        DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-                        outputStream.writeBytes(requestBody);
-                        outputStream.flush();
-                        outputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
-                    String responseCode= String.valueOf(connection.getResponseCode());
-
-                    StringBuilder responseBody;
-                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                        String line;
-
-                        StringBuilder response = new StringBuilder();
-                        while ((line = reader.readLine()) != null) {
-                            response.append(line);
-                        }
-                        token = String.valueOf(new StringBuilder(response.toString())); // Assign the response to the variable
-                    }
-
-
-                    connection.disconnect();
-                } catch (IOException e) {
-                    e.printStackTrace();
-
-                }
-            }
-        });
-
-// Start the thread
-        thread.start();
-
-        try {
-            // Wait for the thread to finish
-            thread.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
-
