8a2cd694e9965e177d5f2d96990fafe25bddc8ad
shulem1997 <shalomadoram@gmail.com>
2023-06-21 17:29:28 +0300

    contacts from server

--- app/src/main/java/com/example/chat/ContactsActivity.java
+++ app/src/main/java/com/example/chat/ContactsActivity.java
@@ -4,21 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import com.example.chat.databinding.ActivityContactsBinding;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class ContactsActivity extends AppCompatActivity {
@@ -26,7 +31,7 @@ public class ContactsActivity extends AppCompatActivity {
     private AppDB db;
     private ListView lvChats;
     private List<String> contacts;
-    private List<Chat> chatList;
+    private ArrayList<Chat> chatList;
     private ArrayAdapter<String> adapter;
     private UserDao userDao;
     private User logged;
@@ -59,7 +64,7 @@ public class ContactsActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        loadChats(logged);
+        loadChatsFromServer(logged);
     }
 
 
@@ -69,7 +74,7 @@ public class ContactsActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts);
 
         loadChats(logged);
-
+        loadChatsFromServer(logged);
         lvChats.setAdapter(adapter);
         lvChats.setOnItemClickListener((adapterView, view, i, l) -> {
             Intent intent = new Intent(this, ChatActivity.class);
@@ -85,17 +90,19 @@ public class ContactsActivity extends AppCompatActivity {
         chatList = userDao.get(logged.getUsername()).getChats();
         for (Chat chat : chatList) {
             contacts.add(chat.getUser().getDisplayName());
-            //Triple t = new Triple();
-            //t.setImg(chat.getUser().getProfilePic());
-            //t.setName(chat.getUser().getDisplayName());
-
         }
 
         adapter.notifyDataSetChanged();
     }
+    private void loadChatsFromServer(User logged) {
+        getChatsFormServer(logged.getUsername(), logged.getPassword());
+        contacts.clear();
+        for (Chat chat : chatList) {
+            contacts.add(chat.getUser().getDisplayName());
+        }
+    }
 
     private void getChatsFormServer(String username, String password) {
-        getToken(username, password);
         Thread thread = new Thread(new Runnable() {
             private StringBuilder responseBody; // Variable to hold the response body
 
@@ -105,6 +112,7 @@ public class ContactsActivity extends AppCompatActivity {
 
             @Override
             public void run() {
+                getToken(username, password);
                 try {
                     URL url = new URL(Settings.getServer() + "/api/Chats/");
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -121,7 +129,14 @@ public class ContactsActivity extends AppCompatActivity {
                             while ((line = reader.readLine()) != null) {
                                 response.append(line);
                             }
-                            chats = response.toString(); // Assign the response to the variable
+                            chats = response.toString();
+                            Gson gson = new Gson();
+
+                            Type arrayListType = new TypeToken<ArrayList<Chat>>() {}.getType();
+
+                            chatList = gson.fromJson(chats, arrayListType);
+
+                            logged.setChats(chatList);
                         }
                     } else {
                         // Handle the error case
@@ -147,8 +162,10 @@ public class ContactsActivity extends AppCompatActivity {
         }
     }
 
-    private void postChatToServer(String username, String password, String chat) {
-        getToken(username, password);
+
+
+    private void getToken(String username, String password) {
+        AtomicInteger responseCode = new AtomicInteger();
         Thread thread = new Thread(new Runnable() {
             private StringBuilder responseBody; // Variable to hold the response body
 
@@ -159,14 +176,14 @@ public class ContactsActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    URL url = new URL(Settings.getServer() + "/api/Chats/"); // Replace with your API endpoint
+                    URL url = new URL(Settings.getServer() + "/api/Tokens/"); // Replace with your API endpoint
 
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("POST");
                     connection.setRequestProperty("Content-Type", "application/json");
                     connection.setDoOutput(true);
 
-                    String requestBody = chat;
+                    String requestBody = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
 
                     try {
                         DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
@@ -177,6 +194,8 @@ public class ContactsActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
 
+
+                    responseCode.set(connection.getResponseCode());
 
                     StringBuilder responseBody;
                     try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
@@ -197,6 +216,7 @@ public class ContactsActivity extends AppCompatActivity {
                 }
             }
         });
+
 // Start the thread
         thread.start();
 
@@ -204,50 +224,13 @@ public class ContactsActivity extends AppCompatActivity {
             // Wait for the thread to finish
             thread.join();
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-    }
+        if (responseCode.get() == 200) {
 
-    private void getToken(String username, String password) {
-        Thread thread = new Thread(new Runnable() {
-            private StringBuilder responseBody; // Variable to hold the response body
-
-            public StringBuilder getResponseBody() {
-                return responseBody;
-            }
-
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL(Settings.getServer() + "/api/Chats/");
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setRequestProperty("Content-Type", "application/json");
-                    connection.setRequestProperty("Authorization", "Bearer " + token);
-
-                    int responseCode = connection.getResponseCode();
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        StringBuilder responseBody;
-                        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                            String line;
-                            StringBuilder response = new StringBuilder();
-                            while ((line = reader.readLine()) != null) {
-                                response.append(line);
-                            }
-                            chats = response.toString(); // Assign the response to the variable
-                        }
-                    } else {
-                        // Handle the error case
-                        System.out.println("HTTP GET request failed with response code: " + responseCode);
-                    }
-
-                    connection.disconnect();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println(e.toString());
-                }
-            }
-        });
+        } else {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+        }
     }
 }
 
