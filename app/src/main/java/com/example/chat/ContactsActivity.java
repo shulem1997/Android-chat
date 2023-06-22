d3345858a6cf128f0ad3fb76652aeef9960320e5
shulem1997 <shalomadoram@gmail.com>
2023-06-22 12:43:15 +0300

    contacts

--- app/src/main/java/com/example/chat/ContactsActivity.java
+++ app/src/main/java/com/example/chat/ContactsActivity.java
@@ -2,11 +2,13 @@ package com.example.chat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import com.example.chat.databinding.ActivityContactsBinding;
@@ -26,13 +28,13 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-public class ContactsActivity extends AppCompatActivity {
+public class ContactsActivity extends AppCompatActivity implements OnItemClickListener{
     private ActivityContactsBinding binding;
     private AppDB db;
-    private ListView lvChats;
-    private List<String> contacts;
+    private RecyclerView lvChats;
+    private List<User> contacts;
     private ArrayList<Chat> chatList;
-    private ArrayAdapter<String> adapter;
+    private ContactAdapter adapter;
     private UserDao userDao;
     private User logged;
     private String token;
@@ -72,25 +74,25 @@ public class ContactsActivity extends AppCompatActivity {
     private void handlePosts() {
         lvChats = binding.lvChats;
         contacts = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts);
-
+        adapter = new ContactAdapter(contacts);
+        adapter.setOnItemClickListener(this);
         loadChats(logged);
         loadChatsFromServer(logged);
         lvChats.setAdapter(adapter);
-        lvChats.setOnItemClickListener((adapterView, view, i, l) -> {
-            Intent intent = new Intent(this, ChatActivity.class);
-            String[] arr = new String[]{logged.getUsername(), chatList.get(i).getIdStr()};
-            intent.putExtra("chatInfo", arr);
-            startActivity(intent);
-        });
 
+    }
+    public void onItemClick(View view, int position) {
+        Intent intent = new Intent(this, ChatActivity.class);
+        String[] arr = new String[]{logged.getUsername(), chatList.get(position).getIdStr()};
+        intent.putExtra("chatInfo", arr);
+        startActivity(intent);
     }
 
     private void loadChats(User logged) {
         contacts.clear();
         chatList = userDao.get(logged.getUsername()).getChats();
         for (Chat chat : chatList) {
-            contacts.add(chat.getUser().getDisplayName());
+            contacts.add(chat.getUser());
         }
 
         adapter.notifyDataSetChanged();
@@ -99,7 +101,7 @@ public class ContactsActivity extends AppCompatActivity {
         getChatsFormServer(logged.getUsername(), logged.getPassword());
         contacts.clear();
         for (Chat chat : chatList) {
-            contacts.add(chat.getUser().getDisplayName());
+            contacts.add(chat.getUser());
         }
         adapter.notifyDataSetChanged();
     }
